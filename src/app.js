7ff028f25195c9ccf6acc1145d10dac965ffb3a0
Gabriellji <gabriellji@yahoo.com>
2021-01-21 00:06:30 +0300

    feat: create prifile resourse

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 const imageRouter = require('./resources/images/image.router')
-
+const profileRouter = require('./resources/profiles/profile.router')
 
 app.use(express.json())
 
@@ -16,5 +16,6 @@ app.use('/', (req, res, next) => {
 })
 
 app.use('/image', imageRouter)
+app.use('/profile', profileRouter)
 
 module.exports = app
